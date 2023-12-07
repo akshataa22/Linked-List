75ff952dd59735c063204c4c77581167b00d470d
akshataa22 <dhanawadeakshata53@gmail.com>
2023-12-08 00:22:11 +0530

    adding 56-30-70 to the list

--- LinkedList.java
+++ LinkedList.java
@@ -5,12 +5,15 @@ public class LinkedList {
     Node tail;
     public void add(int data) {
         Node newNode = new Node(data);
-        if (head == null) {
-            head = tail=newNode;
+        if (head == null){
+            head = tail = newNode;
         }
-        else {
-            tail.next=newNode;
-            tail=newNode;
+        else{
+            Node temp = head;
+            while (temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
         }
     }
 
